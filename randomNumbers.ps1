674a996d3eb223b46ad816b25e624f03e6e462cc
Koleen BP <koleen.bp@outlook.com>
2018-01-09 00:01:39 +0800

    CLEAN-UP

--- randomNumbers.ps1
+++ randomNumbers.ps1
@@ -2,14 +2,6 @@
 #@KBPsystem777
 #RandomNumbers
 
-# =======================================================
-
-#TEST #-join (1..5 | %{ [char[]](0..127) -match ‘p{L}’ | Get-Random })
-#TEST #-join ("2017" + 77..200)| Get-Random -Count 10
-#TEST #cd C:\users\koleen.b.paunon\Documents
-#TEST #$RandomKeys = -join ((777..7000) + (777..7000) | Get-Random -Count 600 | % {[int]$_})
-#TEST #| Export-Csv -Path "C:\users\USER_NAME\randomkeys.csv";
-
 #$RandomKeys | Export-Csv -Path "C:\users\USER_NAME\randomkeys.csv";
 $Ks = (1000..9999) | Get-Random -Count 1000 | % {"2017"+"-"+[int]$_};
 
@@ -19,6 +11,3 @@ $Ks;
 #Export Results
 $Ks | Export-Csv -Path "C:\Users\USER_NAME\Ks.csv";
 
-
-
-#========================================================
