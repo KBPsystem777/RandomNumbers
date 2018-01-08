244529cba170c48cdbbf757aab44663a6a787708
Koleen BP <koleen.bp@outlook.com>
2018-01-09 00:00:35 +0800

    Rename to randomNumbers.ps1
    PowerShell

--- randomNumbers.ps1
+++ randomNumbers.ps1
@@ -8,16 +8,16 @@
 #TEST #-join ("2017" + 77..200)| Get-Random -Count 10
 #TEST #cd C:\users\koleen.b.paunon\Documents
 #TEST #$RandomKeys = -join ((777..7000) + (777..7000) | Get-Random -Count 600 | % {[int]$_})
-#TEST #| Export-Csv -Path "C:\users\koleen.b.paunon\Downloads\randomkeys.csv";
+#TEST #| Export-Csv -Path "C:\users\USER_NAME\randomkeys.csv";
 
-#$RandomKeys | Export-Csv -Path "C:\users\koleen.b.paunon\Downloads\randomkeys.csv";
+#$RandomKeys | Export-Csv -Path "C:\users\USER_NAME\randomkeys.csv";
 $Ks = (1000..9999) | Get-Random -Count 1000 | % {"2017"+"-"+[int]$_};
 
 #Print Variables
 $Ks;
 
 #Export Results
-$Ks | Export-Csv -Path "C:\Users\koleen.b.paunon\Downloads\Ks.csv";
+$Ks | Export-Csv -Path "C:\Users\USER_NAME\Ks.csv";
 
 
 
